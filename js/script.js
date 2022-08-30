950fb3048d57456de178d4410094858b26293e09
Khash <khashayar.m.003@gmail.com>
2022-08-30 08:45:22 +0430

    making buttons

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ function showPage(list,page){
         for(let i=0 ; i<list.length ; i++){
             if(i>=startIndex && i<endIndex){
                studentItem += `
-               <li class="student-item cf">
+              <li class="student-item cf">
                <div class="student-details">
                  <img class="avatar" src= ${list[i].picture.large} alt="Profile Picture">
                  <h3>${list[i].name.first} ${list[i].name.last}</h3>
@@ -37,19 +37,58 @@ function showPage(list,page){
                <div class="joined-details">
                  <span class="date">Joined ${list[i].registered.date}</span>
                </div>
-             </li>
+              </li>
                `;
             }
         }
         studentList.insertAdjacentHTML('beforeend', studentItem);
 }
-showPage(data,4);
+// showPage(data,4);
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
+function addPagination(list){
+  
+   // create a variable to calculate the number of pages needed
+   const numOfPages = Math.ceil(list.length / 9);
+  // select the element with a class of `link-list` and assign it to a variable
+   const linkList = document.querySelector('.link-list');
+  // set the innerHTML property of the variable you just created to an empty string
+   linkList.innerHTML = '';
+   let buttonsPage = '';
+  // loop over the number of pages needed
+    // create the elements needed to display the pagination button
+    // insert the above elements
+   for(let i=1; i<numOfPages.length ; i++) {
+      
+      buttonsPage += `
+      <li>
+      <button type="button">${i}</button>
+      </li>`;
+   }
+   linkList.insertAdjacentHTML('beforeend', buttonsPage);
+  // give the first pagination button a class of "active"
+  // create an event listener on the `link-list` element
+    // if the click target is a button:
+      // remove the "active" class from the previous button
+      // add the active class to the clicked button
+      // call the showPage function passing the `list` parameter and page to display as arguments
+      const button = document.querySelector("button[type='button']")
+      button[0].classList.add('active');
+      linkList.addEventListener('click', (event) => {
+         const clicked = event.target;
+          if (clicked.tagName === 'BUTTON'){
+            const activeButton = document.getElementsByClassName('active');
+            activeButton[0].classList.remove('active');
+            clicked.classList.add('active');
+            showPage(list);
+          }
+        })
+}
 
 
 // Call functions
+showPage(data,1)
+addPagination(data)
